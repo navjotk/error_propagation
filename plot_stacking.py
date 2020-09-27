47a41d2aa7f465e3a32694eab3e6cec5403c6d7f
Navjot Kukreja <navjotk@gmail.com>
2020-09-27 13:12:33 +0100

    Add plotter for stacking experiment

---
+++ plot_stacking.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+stacking_results = pd.read_csv("stacking_experiment_results.csv")
+
+results_1 = stacking_results[stacking_results["atol"]==1e-1][["shot", "angle"]].sort_values("shot")
+results_4 = stacking_results[stacking_results["atol"]==1e-4][["shot", "angle"]].sort_values("shot")
+results_8 = stacking_results[stacking_results["atol"]==1e-8][["shot", "angle"]].sort_values("shot")
+results_16 = stacking_results[stacking_results["atol"]==1e-16][["shot", "angle"]].sort_values("shot")
+
+plt.plot(results_1["shot"], results_1["angle"], "-", label="atol=1e-1")
+plt.plot(results_4["shot"], results_4["angle"], "--", label="atol=1e-4")
+plt.plot(results_8["shot"], results_8["angle"], ":", label="atol=1e-8")
+plt.plot(results_16["shot"], results_16["angle"], ".-", label="atol=1e-16")
+plt.xlabel("shots")
+plt.ylabel("Angle with perfect gradient (radians)")
+plt.title("Angular deviation of gradient as a function of number of shots stacked")
+plt.legend()
+plt.savefig("stacking_experiment_results.pdf", bbox_inches='tight')
